4c67460020214769240a62cb9368cea70e592aa7
NotoriousRGB <info@notoriousrgb.com>
2012-03-08 03:40:37 +1100

    New blog section

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
 
+
 get '/' do
   @title = "Neptune - A Wordpress Theme"
   erb :index
+end
+
+get '/about' do
+  @title = "About"
+  erb :page_about
+end
+
+get '/portfolio' do
+  @title = "About"
+  erb :page_portfolio
 end
 
 __END__
